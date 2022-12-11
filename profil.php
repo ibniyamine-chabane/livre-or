a7018ef7a06f3ca3d91ec5acd45c3c0e0d77913a
ibniyamine-chabane <ibni-yamine.chabane@laplateforme.io>
2022-12-11 15:16:45 +0100

    profil.php leger changement de la font a monospace page fini

--- profil.php
+++ profil.php
@@ -26,7 +26,7 @@
         //var_dump($user_info);
         $login_prefilled = $user_info[0][0];
 
-/*
+            // formulaire de changement de login
         if (isset($_POST['submit_login'])) {
 
             if ( $_POST['login'] && $_POST['current_password'] ) {
@@ -61,9 +61,9 @@
             } else {
                 $message = "vous devez remplir tous les champ !";
             }
-        }*/
+        }
 
-        
+            // formulaire de changement de mot de passe
         if (isset($_POST['submit_pass'])) {
 
             if ( $_POST['current_password2'] && $_POST['new_password'] && $_POST['password_confirm'] ) {
@@ -71,35 +71,40 @@
                 $current_password = trim($_POST['current_password2']); 
                 $new_password   = trim($_POST['new_password']);
                 $password_confirm = trim($_POST['password_confirm']);
-                $change_password_ok = false;
-                $current_password_check = false;
+                $change_password_ok = "non";
+                $current_password_check = "non";
 
 
                 foreach ($user_info as $user_password_db) {
-                    var_dump($user_password_db);
+                    //var_dump($user_password_db);
                     if ( $current_password != $user_password_db[1] ) {
-                        $display_msg = '<p style ="color:red">mot de passe actuel invalid!</p>';
+                        $display_msg = '<p style ="color:red; text-align:center">mot de passe actuel invalid!</p>';
+                        $current_password_check = "non";
+                        break;
                     } else {
-                        $display_msg = '<p style ="color:white; text-align:center; font-weight:bold">correspond !</p>';;
-                        $current_password_check = true; 
+                        //$display_msg = '<p style ="color:green; text-align:center; font-weight:bold">mot de passe actuel ok</p>';
+                        $current_password_check = "oui"; 
                     }
 
                     if ( $new_password == $password_confirm ) {
-                        $message_password_display = "les mdp correpond!!";
-                        $change_password_ok = true;
+                        $message_password_display = '<p style ="color:green; text-align:center; font-weight:bold">les mdp correpond</p>';
+                        $change_password_ok = "oui";
+                        $send = $new_password;
                     } else {
+                        $change_password_ok = "non";
                         $message_password_display = '<p style="color: red; text-align: center">la confirmation du nouveau mot de passe ne correspond pas!!</p>';
-                    }
-
-                    //if ()
-
-                    /*if ( $change_password_ok = true && $current_password_check = true ) {
-                        $update = "UPDATE `utilisateurs` SET `password` = '$new_password' WHERE `utilisateurs`.`login` = '$filled'";
-                        $request_change_password = $connectDatabase->query($update);
-                        header("Location:profil.php");
-                    }*/
+                    }        
 
                 }
+
+                
+                    if ( $change_password_ok === "oui" && $current_password_check === "oui" ) {
+                        $update = "UPDATE `utilisateurs` SET `password` = '$send' WHERE `utilisateurs`.`login` = '$filled'";
+                        $request_change_password = $connectDatabase->query($update);
+                        //header("Location:profil.php");
+                        $display_msg = '<p style ="color:green; text-align:center; font-weight:bold">changement validé</p>';
+
+                    }
 
             } else {
                 $message_password_display = '<p style="color: red; text-align: center">vous devez remplir tous les champ mot de passe !</p>';
@@ -129,38 +134,40 @@
     <?php include("header.php"); ?>
     <main>
         <section>
-            <div style="display: flex;">
-                <div class="container-form">
-                    <h1>Votre profil</h1>
-                    <h2>changer de login</h2>
-                    <p class="msg-error"><?= $message ?></p>
-                    <form method="post">
+            <div class="container_profil">
+                <h1 class="profil_title">Profil</h1>
+                <div class="form_flex">
+                    <div class="container-form">
+                        <h2>changer de login</h2>
+                        <p class="msg-error"><?= $message ?></p>
+                        <form method="post">
 
-                        <label for="flogin">Login</label>
-                            <input type="text" name="login" value=<?= $login_prefilled ?> placeholder="Choisissez votre login">
-                        <label for="">renseigner votre mot de passe actuel pour tout changement</label>
-                            <input type="password" name="current_password" placeholder="votre mot de passe actuel">
-                            <input type="submit" name="submit_login" value="valider">
+                            <label for="flogin">Login</label>
+                                <input type="text" name="login" value=<?= $login_prefilled ?> placeholder="Choisissez votre login">
+                            <label for="">renseigner votre mot de passe actuel pour tout changement</label>
+                                <input type="password" name="current_password" placeholder="votre mot de passe actuel">
+                                <input type="submit" name="submit_login" value="valider">
 
-                    </form> 
-                </div>   
+                        </form> 
+                    </div>   
 
-                <div class="container-form">        
-                    <form method='post'>
+                    <div class="container-form">        
+                        <form method='post'>
 
-                        <h2>changer de mot de passe</h2>
-                        <?= $message_password_display ?>
-                        <label for="">renseigner votre mot de passe actuel pour tout changement</label>
-                        <?= $display_msg ?>
-                            <input type="password" name="current_password2" placeholder="votre mot de passe actuel">
-                        <label for="fpassword">Mot de Passe</label>
-                            <input type="password" value="" name="new_password" placeholder="Nouveau mot de Passe">
-                            <input type="password" name="password_confirm" placeholder="Confirmer le mot de Passe">
-                            <input type="submit" name="submit_pass" value="valider">
+                            <h2>changer de mot de passe</h2>
+                            <?= $message_password_display ?><?= $display_msg ?>
+                            <label for="">renseigner votre mot de passe actuel pour tout changement</label>
+                            
+                                <input type="password" name="current_password2" placeholder="votre mot de passe actuel">
+                            <label for="fpassword">Mot de Passe</label>
+                                <input type="password" value="" name="new_password" placeholder="Nouveau mot de Passe">
+                                <input type="password" name="password_confirm" placeholder="Confirmer le mot de Passe">
+                                <input type="submit" name="submit_pass" value="valider">
 
-                    </form>
-                </div>    
-            </div>
+                        </form>
+                    </div>    
+                </div>
+            </div>    
         </section> 
     </main>
 </body>
